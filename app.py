a9b23f5adebe1173219c46a15affbeaa1741d097
Rakesh163-213 <rakeshsheokand97280@gmail.com>
2026-05-03 19:42:52 +0530

    Initialize Flask application with home route

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '✅ Flask is running! Bot should be running too.'
+
+if __name__ == "__main__":
+    # Koyeb passes a PORT variable; we use 8000 as a backup
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
